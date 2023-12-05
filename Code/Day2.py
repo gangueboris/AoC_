fd1841e00395ca31ccd13d45c845e2acf908640f
gangueboris <gangueboris@gmail.com>
2023-12-05 14:08:06 +0100

    update

--- Code/Day2.py
+++ Code/Day2.py
@@ -1,23 +1,51 @@
-with open("Doc&images\input_day2.txt","r") as file:
-    file = file.read()
+import numpy as np
+with open("test.txt", "r") as file:
+    content = file.read()
 
-letterList = ["one","two","three","four","five","six","seven","eight","nine"]
-digitList = [0,1,2,3,4,5,6,7,8,9]
+word_to_digit = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
+lines = content.split("\n")
+result = []
 
-lines = file.split("\n")
-index = 0 
-count = 0
-digitIndex = 0
 for line in lines:
-    if line not in lines:
+    if not line:
         continue
-    for i in range(len(letterList)):
-        while index < len(line):
-            index = line.find(letterList[i],index) # la methode find me renvoi -1 si le sous-charactere n'a pas été trouvé
-            if index == -1: break   
-            count += 1
-            digitIndex = i
-            index += 1 # Pour continue la recherche à partir du charactere suivant sinon l'algo va repeter la même chose
-        count = 0    
-      
+
+    digits = [int(char) for char in line if char.isdigit()]
+    indexDigit = [line.index(char) for char in line if char.isdigit()]
+
+    words = []
+    index_word = []
+
+    for word, value in word_to_digit.items():
+        index = line.find(word)
+        while index != -1:
+            words.append(value)
+            index_word.append(index)
+            index = line.find(word, index + 1)
+
+    if index_word and indexDigit:
+        if indexDigit[0] < index_word[0] and indexDigit[-1] < index_word[-1]:
+            ab = int(str(digits[0]) + str(words[-1]))
+            result.append(ab)
+        elif index_word[0] < indexDigit[0] and index_word[-1] < indexDigit[-1]:
+            ab = int(str(words[0]) + str(digits[-1]))
+            result.append(ab)
+        elif indexDigit[0] < index_word[0] and indexDigit[-1] > index_word[-1]:
+            ab = int(str(digits[0]) + str(digits[-1]))
+            result.append(ab)
+        elif indexDigit[0] > index_word[0] and indexDigit[-1] < index_word[-1]:
+            ab = int(str(words[0]) + str(words[-1]))
+            result.append(ab)
+    elif not index_word and indexDigit:
+        ab = int(str(digits[0]) + str(digits[-1]))
+        result.append(ab)
+    elif index_word and not indexDigit:
+        ab = int(str(words[-1]) + str(words[1]))
+        result.append(ab)
+    else:
+        result.append(0)
+print(result)
+result = np.sum(result)
+print(result)
+ 
