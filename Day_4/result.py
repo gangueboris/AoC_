3d9f79a5f4f32dd2debbc2a6e652b091dedbcce3
gangueboris <gangueboris@gmail.com>
2023-12-13 15:59:52 +0100

    new

--- Day_4/result.py
+++ Day_4/result.py
@@ -5,15 +5,19 @@ content = content.split("\n")
 
 cards = [content[i].split(':')[0] for i in range(len(content))]
 others = [content[i].split(':')[1] for i in range(len(content))]
-wins = [[val for val in others[i].split('|')[0]] for i in range(len(others))]
-data = [[val for val in others[i].split('|')[1]] for i in range(len(others))]
+wins = [[c for c in others[i].split('|')[0].split()] for i in range(len(others))]
+data = [[c for c in others[i].split('|')[1].split()] for i in range(len(others))]
 
-print(wins)
-"""
-init = 1
-for i in range(len(data)):
-    for j in range(len(wins)):
-        if data[i]
-"""
-
-
+result = 0
+for i, j in zip(data,wins):  
+    point = 0
+    for w in i: 
+        if w in j:
+           if point<1:  
+              point += 1
+           else:
+              point *= 2
+    result += point      
+   
+print(result)
+    
