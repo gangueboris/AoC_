11738a79f78a6c64650478633b2872e4631d550b
gangueboris <gangueboris@gmail.com>
2024-07-02 02:09:15 +0200

    D-4 part2 complete

--- Day_4/part_2.py
+++ Day_4/part_2.py
@@ -1,4 +1,4 @@
-content = open("Day_4/text.txt", 'r').read().splitlines()
+content = open("Day_4/input.txt", 'r').read().splitlines()
 """
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -9,23 +9,25 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 
 An instance card: is
 Goal: Sum up the instances cards and return the result.
+If we remark well, we have a cumulative operation over here
 """
 
 def totalInstances(content):
-    temp = {i:0 for i in range(1, len(content)+1)}
-    for line in content:
+    temp = {i:1 for i in range(len(content))} # handle original copy
+
+    for i, line in enumerate(content):
         count = 0
-        card, numbers = line.split(':')
-        _, n = card.split()
-        winning, mine = numbers.split('|')
+        winning, mine = line.split(':')[1].split('|')
         winning = winning.split()
         mine = mine.split()
         for v in mine:
             if v in winning:
                 count += 1
-        
-        num = [str(c) for c in range(int(n)+1, count+2)]
-        print(num)
-        
     
-totalInstances(content)
+        for j in range(count):
+            temp[i+j+1] += temp[i]
+
+    return sum(temp.values())
+          
+print(totalInstances(content)) #  6227972
+ 
